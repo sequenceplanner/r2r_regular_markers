c903b1a9a42bb444c427c068dc1134e69b449652
endre90 <endre90@gmail.com>
2024-11-14 12:01:20 +0100

    can insert marker

---
+++ examples/simple_marker.rs
@@ -0,0 +1,183 @@
+use r2r::geometry_msgs::msg::{Point, Pose, Quaternion, Transform, TransformStamped, Vector3};
+use r2r::std_msgs::msg::Header;
+use r2r::tf2_msgs::msg::TFMessage;
+use r2r::visualization_msgs::msg::Marker;
+use r2r::Context;
+use r2r::QosProfile;
+use r2r_regular_markers::RegularMarkerServer;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+pub static NODE_ID: &'static str = "simple_marker";
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct FrameData {
+    pub parent_frame_id: String,
+    pub child_frame_id: String,
+    pub transform: Transform,
+    pub active: Option<bool>,
+}
+
+fn make_initial_tf() -> HashMap<String, FrameData> {
+    let mut test_setup = HashMap::<String, FrameData>::new();
+
+    test_setup.insert(
+        "base_link".to_string(),
+        FrameData {
+            parent_frame_id: "world".to_string(),
+            child_frame_id: "base_link".to_string(),
+            transform: r2r::geometry_msgs::msg::Transform {
+                translation: r2r::geometry_msgs::msg::Vector3 {
+                    x: 0.0,
+                    y: 0.0,
+                    z: 1.0,
+                },
+                rotation: r2r::geometry_msgs::msg::Quaternion {
+                    x: 0.0,
+                    y: 0.0,
+                    z: 0.0,
+                    w: 1.0,
+                },
+            },
+            active: Some(false),
+        },
+    );
+    test_setup
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let context = Context::create()?;
+    let node = r2r::Node::create(context, "simple_marker", "")?;
+    let arc_node = Arc::new(Mutex::new(node));
+
+    // We need to publish a frame where the marker can be placed
+    let broadcasted_frames = Arc::new(Mutex::new(make_initial_tf()));
+    let arc_node_clone = arc_node.clone();
+    let static_pub_timer = arc_node_clone
+        .lock()
+        .unwrap()
+        .create_wall_timer(std::time::Duration::from_millis(20))?;
+    let static_frame_broadcaster = arc_node_clone
+        .lock()
+        .unwrap()
+        .create_publisher::<TFMessage>(
+            "tf_static",
+            QosProfile::transient_local(QosProfile::default()),
+        )?;
+    let broadcasted_frames_clone = broadcasted_frames.clone();
+    tokio::task::spawn(async move {
+        match static_frame_broadcaster_callback(
+            static_frame_broadcaster,
+            static_pub_timer,
+            &broadcasted_frames_clone,
+        )
+        .await
+        {
+            Ok(()) => (),
+            Err(e) => r2r::log_error!(NODE_ID, "Static frame broadcaster failed with: '{}'.", e),
+        };
+    });
+
+    let arc_node_clone = arc_node.clone();
+    let server = RegularMarkerServer::new("", "simple_marker", arc_node_clone);
+
+    let mut marker = Marker::default();
+    marker.header.frame_id = "base_link".to_string();
+    marker.type_ = Marker::CUBE as i32;
+    marker.scale.x = 0.45;
+    marker.scale.y = 0.45;
+    marker.scale.z = 0.45;
+    marker.color.r = 0.0;
+    marker.color.b = 0.5;
+    marker.color.g = 0.5;
+    marker.color.a = 1.0;
+    marker.pose = Pose {
+        position: Point {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        },
+        orientation: Quaternion {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+            w: 1.0,
+        },
+    };
+
+    // Insert the marker into the server
+    server.insert("my_marker", marker);
+
+    // Apply changes to publish updates
+    server.apply_changes();
+
+    // Keep the node alive
+    let arc_node_clone: Arc<Mutex<r2r::Node>> = arc_node.clone();
+    let handle = std::thread::spawn(move || loop {
+        arc_node_clone
+            .lock()
+            .unwrap()
+            .spin_once(std::time::Duration::from_millis(100));
+    });
+
+    r2r::log_info!(NODE_ID, "Node started.");
+
+    handle.join().unwrap();
+
+    Ok(())
+}
+
+// Broadcast static frames
+async fn static_frame_broadcaster_callback(
+    publisher: r2r::Publisher<TFMessage>,
+    mut timer: r2r::Timer,
+    frames: &Arc<Mutex<HashMap<String, FrameData>>>,
+    // node_id: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut x = 0.0;
+    loop {
+        x = x + 0.001;
+        let mut clock = r2r::Clock::create(r2r::ClockType::RosTime).unwrap();
+        let now = clock.get_now().unwrap();
+        let time_stamp = r2r::Clock::to_builtin_time(&now);
+
+        let transforms_local = frames.lock().unwrap().clone();
+        let mut updated_transforms = vec![];
+
+        transforms_local.iter().for_each(|(_, v)| match v.active {
+            Some(false) => {
+                updated_transforms.push(TransformStamped {
+                    header: Header {
+                        stamp: time_stamp.clone(),
+                        frame_id: v.parent_frame_id.clone(),
+                    },
+                    child_frame_id: v.child_frame_id.clone(),
+                    transform: Transform { translation: Vector3 {
+                        x,
+                        y: v.transform.translation.y.clone(),
+                        z: v.transform.translation.z.clone(),
+                    }, rotation: v.transform.rotation.clone(), }
+                    
+                });
+            }
+            Some(true) | None => (),
+        });
+
+        let msg = TFMessage {
+            transforms: updated_transforms,
+        };
+
+        match publisher.publish(&msg) {
+            Ok(()) => (),
+            Err(e) => {
+                r2r::log_error!(
+                    NODE_ID,
+                    "Static broadcaster failed to send a message with: '{}'",
+                    e
+                );
+            }
+        };
+        timer.tick().await?;
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
 use r2r::visualization_msgs::msg::{Marker, MarkerArray};
-use r2r::{Publisher, QosProfile};
+use r2r::{Publisher, QosProfile, Timer};
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
+
+#[derive(Clone, Debug)]
+pub enum UpdateType {
+    Add,
+    Modify,
+    Delete,
+    DeleteAll,
+}
+
+// Struct to hold the information about an update
+#[derive(Clone, Debug)]
+struct UpdateContext {
+    pub update_type: UpdateType,
+    pub marker: Marker,
+}
 
 #[derive(Clone)]
 pub struct RegularMarkerServer {
     pub topic_namespace: String,
     pub topic_name: String,
-    pub markers: Arc<Mutex<HashMap<String, Marker>>>,
-    pub publisher: Publisher<MarkerArray>,
+    marker_contexts: Arc<Mutex<HashMap<String, Marker>>>,
+    pending_updates: Arc<Mutex<HashMap<String, UpdateContext>>>,
+    // update_publisher: Publisher<MarkerArray>
 }
 
 impl RegularMarkerServer {
@@ -23,17 +39,125 @@ impl RegularMarkerServer {
             .create_publisher::<MarkerArray>(&publisher_topic, publisher_qos)
             .expect("Failed to create publisher");
 
-        let markers = Arc::new(Mutex::new(HashMap::new()));
-        let markers_clone = markers.clone();
+        let timer = node
+            .lock()
+            .unwrap()
+            .create_wall_timer(std::time::Duration::from_millis(20))
+            .unwrap();
 
-        // Should probably only publish when there are some changes, test with moving tf frames...
-        // Make and example with some rotating tf frame.
+        let marker_contexts = Arc::new(Mutex::new(HashMap::new()));
+        let pending_updates = Arc::new(Mutex::new(HashMap::new()));
+
+        let marker_contexts_clone = marker_contexts.clone();
+        tokio::task::spawn(async move {
+            match Self::marker_array_publisher(marker_contexts_clone, publisher, timer).await {
+                Ok(()) => (),
+                Err(e) => r2r::log_error!("asdf", "Marker array publisher failed with: '{}'.", e),
+            };
+        });
+
+        let marker_contexts_clone = marker_contexts.clone();
+        let pending_updates_clone = pending_updates.clone();
 
         Self {
             topic_namespace: topic_namespace.to_string(),
             topic_name: topic_name.to_string(),
-            markers: markers_clone,
-            publisher,
+            marker_contexts: marker_contexts_clone,
+            pending_updates: pending_updates_clone,
+        }
+    }
+
+    pub fn insert(&self, name: &str, marker: Marker) {
+        let mut pending_updates = self.pending_updates.lock().unwrap();
+
+        let update_context =
+            pending_updates
+                .entry(name.to_string())
+                .or_insert_with(|| UpdateContext {
+                    update_type: UpdateType::Add,
+                    marker: marker.clone(),
+                });
+
+        update_context.update_type = UpdateType::Add;
+        update_context.marker = marker;
+
+        println!("Marker added with name '{}'", name);
+    }
+
+    pub fn apply_changes(&self) {
+        let mut marker_contexts = self.marker_contexts.lock().unwrap();
+        let mut pending_updates = self.pending_updates.lock().unwrap();
+
+        if pending_updates.is_empty() {
+            println!("No changes to apply");
+            return;
+        }
+
+        for (name, update_context) in pending_updates.iter() {
+            match update_context.update_type {
+                UpdateType::Add => {
+                    marker_contexts.entry(name.clone()).or_insert_with(|| {
+                        let mut marker_context = update_context.marker.clone();
+                        marker_context.action = Marker::ADD as i32;
+                        marker_context
+                    });
+                }
+                UpdateType::Modify => {
+                    if let Some(marker_context) = marker_contexts.get_mut(name) {
+                        marker_context.pose = update_context.marker.pose.clone();
+                        marker_context.header = update_context.marker.header.clone();
+                        marker_context.action = Marker::MODIFY as i32;
+                    } else {
+                        println!("Pending modify update for non-existing marker '{}'.", name);
+                    }
+                }
+                UpdateType::Delete => {
+                    if let Some(marker_context) = marker_contexts.get_mut(name) {
+                        marker_context.action = Marker::DELETE as i32;
+                    } else {
+                        println!("Pending delete update for non-existing marker '{}'.", name);
+                    }
+                }
+                UpdateType::DeleteAll => {
+                    for marker_context in marker_contexts.values_mut() {
+                        marker_context.action = Marker::DELETEALL as i32;
+                    }
+                }
+            }
+        }
+
+        pending_updates.clear();
+    }
+
+    async fn marker_array_publisher(
+        marker_contexts: Arc<Mutex<HashMap<String, Marker>>>,
+        publisher: Publisher<MarkerArray>,
+        mut timer: Timer,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        loop {
+            let mut marker_contexts = marker_contexts.lock().unwrap().clone();
+            let mut update_msg = MarkerArray::default();
+
+            for (_, marker) in &marker_contexts {
+                update_msg.markers.push(marker.clone());
+            }
+
+            publisher
+                .publish(&update_msg)
+                .expect("Failed to publish update");
+
+            // maintain context
+            for (name, marker) in marker_contexts.clone().iter() {
+                match marker.action {
+                    2 => {
+                        let _ = marker_contexts.remove(name);
+                    }
+                    3 => marker_contexts.clear(),
+                    _ => (),
+                }
+            }
+
+            timer.tick().await?;
         }
     }
 }
